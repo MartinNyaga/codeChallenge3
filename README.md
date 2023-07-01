3f3ecd4fa7a781b564d7a8f1f8ba79e7fbe5c162
Martin Nyaga <martin.nyaga@student.moringaschool.com>
2023-07-01 18:27:55 +0300

    Functionality update

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-readme goes here
+# Moringa Movie App(codechallenge3)
+
+#### Created By Martin Nyaga 30-6-2023
+
+## Git Link
+
+[Git](https://github.com/MartinNyaga/codeChallenge3)
+
+## Description
+
+Moringa Movie app is a web app created to showcase available movies in the moringa school database and be able to buy the movie tickets
+
+## Setup Requirements
+
+- Git
+- Web-browser or your choice
+- Github
+- json server
+
+
+## Development server
+
+Run `json-server --watch db.json` for the server. Navigate to `http://localhost:3000/films/` and the app will run on the browser. 
+
+
+## Technologies Used
+
+The following have been used on this project:
+
+- HTML
+- CSS
+- JavaScript
+
+
+## Screenshots 
+
+- Homepage
+
+  <img src="" alt="screenshot" />
+
+
+
+
+## Known Bugs
+
+No known bugs at the moment
+
+## Support and contact details 
+
+To make a contribution to the code used or any suggestions you can click on the contact link and email me your suggestions.
+
+- Email: martin.nyaga@student.moringaschool.com
+
+## License
+
+Copyright (c) {{ 2023 }} {{ Martin Mugendi Nyaga }}
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
--- script/index.js
+++ script/index.js
@@ -1,1 +1,79 @@
-//java script code
+// Getting the data from server
+function getData() {
+    fetch('http://localhost:3000/films')
+      .then(response => response.json())
+      .then(data => domManipulate(data));
+  }
+  
+  getData();
+
+  // The DOM manipulation
+
+  function domManipulate(movie){
+    let card = document.getElementById('childOne');
+    let div = document.getElementById('elementOne');
+
+    for(let dub of movie){
+        let dock = document.createElement('li');
+        dock.innerText = dub.title;
+    card.appendChild(dock);
+
+    dock.addEventListener('click', () => {
+      remInfo(); 
+      moreInfo(dub, div);
+    });
+
+     //Creating the voting button and event listener
+     let buyTicket = document.createElement('button');
+     let tickets = dub.tickets_sold;
+     let capacity = dub.capacity;
+     buyTicket.textContent = 'Buy Ticket'
+     dock.appendChild(buyTicket)
+     buyTicket.addEventListener('click', () => {
+       if(tickets >= capacity){
+        alert("The movie is sold out")
+       }else{
+        tickets++
+        updateTickets(dub.id, tickets);
+       }
+
+     })
+    }
+  }
+
+  // Remove displayed information after clicking another name
+function remInfo() {
+    let infoElements = document.getElementsByClassName('info');
+    while (infoElements.length > 0) {
+      infoElements[0].remove();
+    }
+  }
+
+  // Update votes on the server
+function updateTickets(id, tickets) {
+    fetch(`http://localhost:3000/films/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tickets_sold: tickets }),
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      
+      
+  }
+
+  // Getting more information from DOM
+function moreInfo(domInfo, div) {
+    let info = document.createElement('li');
+    info.classList.add('info');
+    info.innerHTML = `<img src="${domInfo.poster}">
+                      <h3>${domInfo.title}</h3>
+                      <p>Description: ${domInfo.description}</p>
+                      <p>Runtime: ${domInfo.runtime}</p>
+                      <p>Showtime: ${domInfo.showtime}</p>
+                      <p>Capacity: ${domInfo.capacity}</p>
+                      <p>Tickets sold: ${domInfo.tickets_sold}</p>`;
+    div.appendChild(info);
+  }
